3c6241319be45667af5688d01c13722394fbfffd
Maxim Starostyn <maximstarostyn@gmail.com>
2025-05-26 12:24:32 +0300

    v1.4.urls++

---
+++ shop/urls.py
@@ -0,0 +1,13 @@
+# shop/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('products/', views.product_list, name='product_list'),
+    path('products/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('cart/', views.cart_detail, name='cart_detail'),
+    path('cart/add/', views.cart_add, name='cart_add'),
+    path('cart/remove/', views.cart_remove, name='cart_remove'),
+    path('checkout/', views.checkout, name='checkout'),
+]
